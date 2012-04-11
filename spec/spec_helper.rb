0557c0fd3c134f9e560c79feabc36c4655dcaba0
Mike Gilbert <mgilbert@on-site.com>
2012-04-11 05:05:06 -0700

    Improved guard/spork operation in project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,6 @@ Spork.prefork do
   # need to restart spork for it take effect.
   ENV["RAILS_ENV"] = "test"
   require File.expand_path("../../config/environment", __FILE__)
-  # Hack to ensure models get reloaded by Spork - remove as soon as this is fixed in Spork.
-  # Silence warnings to avoid all the 'warning: already initialized constant' messages that
-  # appear for constants defined in the models.
-  silence_warnings do
-    Dir["#{Rails.root}/app/models/**/*.rb"].each {|f| load f}
-  end
   Dir[Rails.root.join("spec/support/*.rb")].each { |f| require f }
   require 'rspec/rails'
   RSpec.configure do |config|
@@ -26,10 +20,18 @@ Spork.prefork do
     config.formatter = :documentation # :progress, :html, :textmate
     config.treat_symbols_as_metadata_keys_with_true_values = true
     config.filter_run :focus => true
+    config.filter_run_excluding :wip => true
     config.run_all_when_everything_filtered = true
   end
 end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
+
+  # Hack to ensure models get reloaded by Spork - remove as soon as this is fixed in Spork.
+  # Silence warnings to avoid all the 'warning: already initialized constant' messages that
+  # appear for constants defined in the models.
+  silence_warnings do
+    Dir["#{Rails.root}/app/models/**/*.rb"].each { |f| load f }
+  end
 end
