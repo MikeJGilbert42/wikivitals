b02d3dd7ce6792a926bc6e5e5a842344e460b7be
Mike Gilbert <mgilbert@on-site.com>
2012-04-11 16:23:40 -0700

    Added some lines that I might need from the generated spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,13 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] = "test"
+  ENV["RAILS_ENV"] ||= "test"
   require File.expand_path("../../config/environment", __FILE__)
-  Dir[Rails.root.join("spec/support/*.rb")].each { |f| require f }
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
   require 'rspec/rails'
+  require 'rspec/autorun'
   RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
     # Use color in STDOUT
     config.color_enabled = true
     # Use color not only in STDOUT but also in pagers and files
@@ -22,6 +24,15 @@ Spork.prefork do
     config.filter_run :focus => true
     config.filter_run_excluding :wip => true
     config.run_all_when_everything_filtered = true
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    # instead of true.
+    config.use_transactional_fixtures = true
+
+    # If true, the base class of anonymous controllers will be inferred
+    # automatically. This will be the default behavior in future versions of
+    # rspec-rails.
+    config.infer_base_class_for_anonymous_controllers = false
   end
 end
 
