9afe197d4bb4af6d3e270a528133595ea3bfbc79
Mike Gilbert <mjgilbert@gmail.com>
2012-10-21 10:17:58 -0700

    Do module the right way for using method in classes *and* instances.

--- lib/wiki_helper.rb
+++ lib/wiki_helper.rb
@@ -1,14 +1,20 @@
 module WikiHelper
   extend ActiveSupport::Concern
-  # Repair common mistakes found in case and formatting.
+  module ClassMethods
+    # Repair common mistakes found in case and formatting.
+    def repair_link link
+      return nil if link.nil?
+      # Capitalize all words except of, the, and, excluding words inside parentheses
+      link.gsub!("_", " ")
+      link =~ /([^\(]*)(\([^\)]*\))?/
+      name, parenthetical = Regexp.last_match[1..2]
+      name.gsub!(/\w+/) { |s| s[0] = s[0].upcase; s }
+      name.gsub!(/The|And|Of/) { $&.downcase }
+      name.concat(parenthetical || "").gsub(" ", "_")
+    end
+  end
+
   def repair_link link
-    return nil if link.nil?
-    # Capitalize all words except of, the, and, excluding words inside parentheses
-    link.gsub!("_", " ")
-    link =~ /([^\(]*)(\([^\)]*\))?/
-    name, parenthetical = Regexp.last_match[1..2]
-    name.gsub!(/\w+/) { |s| s[0] = s[0].upcase; s }
-    name.gsub!(/The|And|Of/) { $&.downcase }
-    name.concat(parenthetical || "").gsub(" ", "_")
+    self.class.repair_link link
   end
 end
--- spec/lib/wiki_helper_spec.rb
+++ spec/lib/wiki_helper_spec.rb
@@ -1,11 +1,13 @@
+include WikiHelper
+
 describe WikiHelper do
   describe "#repair_link" do
     it "repairs the links I've encountered that have broken stuff" do
-      WikiHelper::repair_link("Abraham Lincoln").should == "Abraham_Lincoln"
-      WikiHelper::repair_link("Archduke_Franz_Ferdinand_Of_Austria").should == "Archduke_Franz_Ferdinand_of_Austria"
-      WikiHelper::repair_link("Jack White (musician)").should == "Jack_White_(musician)"
-      WikiHelper::repair_link("Catherine Zeta-Jones").should == "Catherine_Zeta-Jones"
-      WikiHelper::repair_link("John DeLorean").should == "John_DeLorean"
+      repair_link("Abraham Lincoln").should == "Abraham_Lincoln"
+      repair_link("Archduke_Franz_Ferdinand_Of_Austria").should == "Archduke_Franz_Ferdinand_of_Austria"
+      repair_link("Jack White (musician)").should == "Jack_White_(musician)"
+      repair_link("Catherine Zeta-Jones").should == "Catherine_Zeta-Jones"
+      repair_link("John DeLorean").should == "John_DeLorean"
     end
   end
 end
