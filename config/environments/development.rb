89ae110c0eb0204daa2c551ecf378d7355266ca4
Mike Gilbert <mjgilbert@gmail.com>
2012-10-23 10:44:20 -0700

    Added Pry and some other gems for fun; make the rails console use Pry.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,9 @@
 Wikivitals::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  silence_warnings do
+    require 'pry'
+    IRB = Pry
+  end
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
