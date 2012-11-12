17191a6b2f5e2dba4b8d404dd05483b11c90b6a8
Mike Gilbert <mjgilbert@gmail.com>
2012-11-11 16:53:30 -0800

    Change exeptions to have configurable messages.

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,25 +1,41 @@
 module Exceptions
   class WikipediaArticleError < StandardError
+    def initialize message = "There was a problem with the article"
+      @message = message
+    end
+
     def message
-      "There was a problem with the article."
+      @message
     end
   end
 
   class ArticleNotFound < WikipediaArticleError
+    def initialize(message = "Article not found")
+      @message = message
+    end
+
     def message
-      "Article not found."
+      @message
     end
   end
 
   class ArticleNotPerson < WikipediaArticleError
+    def initialize(message = "Article is not of a person")
+      @message = message
+    end
+
     def message
-      "Article is not of a person."
+      @message
     end
   end
 
   class WikiRecordStateError < StandardError
+    def initialize message = "Operation not possible before fetching article"
+      @message = message
+    end
+
     def message
-      "Operation not possible before fetching article."
+      @message
     end
   end
 end
