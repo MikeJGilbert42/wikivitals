a82bf471c0d82c076853e575c3648aaa7e44b977
Mike Gilbert <mjgilbert@gmail.com>
2012-11-16 12:27:29 -0800

    Don't use gsub! since the result is a new goddamn string

--- lib/wiki_helper.rb
+++ lib/wiki_helper.rb
@@ -1,20 +1,27 @@
 module WikiHelper
+  include ActiveSupport::Inflector
   extend ActiveSupport::Concern
   module ClassMethods
     # Repair common mistakes found in case and formatting.
     def repair_link link
       return nil if link.nil?
+      link = fix_case link.gsub("_", " ")
+      link.gsub(" ", "_")
+    end
+
+    def humanize_article_title link
+      fix_case link.gsub("_", " ")
+    end
+
+    private
+
+    def fix_case link
       # Capitalize all words except of, the, and, excluding words inside parentheses
-      link.gsub!("_", " ")
       link =~ /([^\(]*)(\([^\)]*\))?/
       name, parenthetical = Regexp.last_match[1..2]
       name.gsub!(/\w+/) { |s| s[0] = s[0].upcase; s }
       name.gsub!(/The|And|Of/) { $&.downcase }
-      name.concat(parenthetical || "").gsub(" ", "_")
-    end
-
-    def humanize_article_title link
-      link.humanze
+      name.concat(parenthetical || "")
     end
   end
 
