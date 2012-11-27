0c21fc536e2904c533f4abd3bbfcb95b375d3cff
Mike Gilbert <mjgilbert@gmail.com>
2012-11-26 17:27:35 -0800

    Change color generator to use all 360 HSL degrees.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def user_color
-    session[:color] ||= Color::HSL.new(rand(359), 60, 60).to_rgb.html
+    session[:color] ||= Color::HSL.new(rand(360), 60, 60).to_rgb.html
   end
 end
