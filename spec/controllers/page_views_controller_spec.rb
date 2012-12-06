c3962fdf2798b60f70b2b90f10b5fe55e897baa2
Mike Gilbert <mjgilbert@gmail.com>
2012-12-05 20:11:18 -0800

    Clean up the PageViewsController spec, remove focus, pry, unreadability

--- spec/controllers/page_views_controller_spec.rb
+++ spec/controllers/page_views_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe PageViewsController, :focus => true do
+describe PageViewsController do
   render_views
 
   let (:user1) { FactoryGirl.create :user }
@@ -11,6 +11,7 @@ describe PageViewsController, :focus => true do
       FactoryGirl.create :page_view, :user => user2
     end
   end
+
   describe "Get recent (JSON)" do
     before do
       get :recent, :format => 'JSON'
@@ -18,7 +19,7 @@ describe PageViewsController, :focus => true do
     it "returns a parsable JSON array of recent page views" do
       expect { JSON.parse(response.body) }.to_not raise_error
     end
-    it "contains the data we expect" do
+    it "contains some of the data we expect" do
       hash = JSON.parse response.body
       hash["page-views"].first.should have_key "color"
       hash["page-views"].first.should have_key "record"
@@ -28,13 +29,13 @@ describe PageViewsController, :focus => true do
 
   describe "Get since (JSON)" do
     before do
-      get :since, :format => 'JSON', :id => 5
+      @id = PageView.all[PageView.count / 2].id
+      get :since, :format => 'JSON', :id => @id
     end
     it "returns only the newest 5 page views" do
-      JSON.parse(response.body)["page-views"].map { |view| view["page-view-id"].to_i }.each do |x|
-        x.should > 5
+      JSON.parse(response.body)["page-views"].map do |view|
+        view["page-view-id"].to_i.should > @id
       end
-      binding.pry
     end
   end
 end
