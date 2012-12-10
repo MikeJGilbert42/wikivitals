25b2b4f134c5290845d524378c727fc73dfffbe8
Mike Gilbert <mjgilbert@gmail.com>
2012-12-09 23:37:46 -0800

    Put cursor into prompt pane.

--- script/tmux-launch.sh
+++ script/tmux-launch.sh
@@ -3,5 +3,6 @@ tmux new-session -d -s wikivitals
 tmux rename-window 'Wikivitals'
 tmux split-window -v 'bundle exec guard'
 tmux split-window -h 'rails s'
+tmux select-pane -t 0
 tmux -2 attach-session -t wikivitals
 
